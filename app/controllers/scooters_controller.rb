f22656653933e6c7f21b00b48fe732aba31c48c6
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-05-27 19:42:08 +0800

    added cloudinary, carrierwave gem. added photo, name column to scooters table. rendered partial for new scooter. added .env file

--- app/controllers/scooters_controller.rb
+++ app/controllers/scooters_controller.rb
@@ -12,14 +12,19 @@ class ScootersController < ApplicationController
   end
 
   def create
+    @user = User.find(1)
     @scooter = Scooter.new(scooter_params)
-    @scooter.save
-    redirect_to scooter_path(@scooter)
+    @scooter.user = @user
+    if @scooter.save
+      redirect_to scooter_path(@scooter)
+    else
+      render :new
+    end
   end
 
   private
 
   def scooter_params
-    params.require(:scooter).permit(:description, :status, :location)
+    params.require(:scooter).permit(:description, :status, :location, :photo, :name)
   end
 end
