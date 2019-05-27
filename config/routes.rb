9fd5f8289bfadf26cf5a42e399fe02058b26b82c
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-05-27 16:06:22 +0800

    made routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :scooters, only: [:index, :show, :new, :create] do
+    resource :reviews, only: [:create]
+  end
+  resources :users, only: [] do
+    resources :bookings, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
