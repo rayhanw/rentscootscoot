ea3099fd117b31d60b431b73e9a8e07307313754
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-05-31 14:15:02 +0800

    scooter status changes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,6 +8,9 @@ class Booking < ApplicationRecord
   validates :scooter, presence: true
   validate :not_blank_date
 
+  after_create :change_scooter_status
+  after_destroy :change_back_scooter_status
+
   private
 
   def not_blank_date
@@ -15,5 +18,17 @@ class Booking < ApplicationRecord
       errors.add(:start_date, "can't be in the past") if start_date.to_date < Date.today
       errors.add(:start_date, "can't be after the end date") if start_date.to_date > end_date
     end
+  end
+
+  def change_scooter_status
+    scooter = Scooter.find(scooter_id)
+    scooter.status = 'Unavailable'
+    scooter.save
+  end
+
+  def change_back_scooter_status
+    scooter = Scooter.find(scooter_id)
+    scooter.status = 'Available'
+    scooter.save
   end
 end
